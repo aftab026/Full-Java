64b4e1277e8dcaa75690d6ec0bbb2dcf67dd0b2b
Aftab <110347957+aftab026@users.noreply.github.com>
2023-02-22 05:46:03 +0530

    Add files via upload

---
+++ 12. Constructors/DefaultArgument.java
@@ -0,0 +1,38 @@
+// q:- 01. Write a class with a default constructor, one argument constructor and two argument
+//   constructors. Instantiate the class to call all the constructors of that class from
+//   a main class
+
+class ParentClass_01 {
+    int rollNo;
+    String name;
+    String branch;
+
+    // default constructor
+    ParentClass_01() {
+        System.out.println("Student Details");
+    }
+
+    // one argument constructor
+    ParentClass_01(int rollNo) {
+        this.rollNo = rollNo;
+        System.out.println("Roll No : " + this.rollNo);
+    }
+
+    // two argument constructor
+    ParentClass_01(String name, String branch) {
+        this.name = name;
+        this.branch = branch;
+        System.out.println("Name : " + this.name);
+        System.out.println("Branch : " + this.branch);
+    }
+}
+
+public class DefaultArgument {
+    public static void main(String[] args) {
+        // calling constructors from main class
+        // creating objects and passing values
+        new ParentClass_01();
+        new ParentClass_01(553);
+        new ParentClass_01("Aftab", "CSE");
+    }
+}
